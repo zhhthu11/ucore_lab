b94aedec2186797796c8981fc294df38489ef3c5
Honghui Zhang <zhh_thu@163.com>
2015-05-08 11:24:11 +0800

    labX add lab5 code

--- labcodes/lab8/kern/process/proc.c
+++ labcodes/lab8/kern/process/proc.c
@@ -106,7 +106,7 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
-     //LAB5 YOUR CODE : (update LAB4 steps)
+     //LAB5 2011011237 : (update LAB4 steps)
     /*
      * below fields(add in LAB5) in proc_struct need to be initialized	
      *       uint32_t wait_state;                        // waiting state
@@ -128,13 +128,15 @@ alloc_proc(void) {
         proc->runs = 0;
         proc->kstack = 0;
         proc->need_resched = 0;
-        proc->parent = 0;
-        proc->mm = 0;
+        proc->parent = NULL;
+        proc->mm = NULL;
         memset(&(proc->context), 0, sizeof(struct context));
-        proc->tf = 0;
+        proc->tf = NULL;
         proc->cr3 = boot_cr3;
         proc->flags = 0;
         memset(proc->name, 0, PROC_NAME_LEN);
+        proc->wait_state = 0;
+        proc->cptr = proc->optr = proc->yptr = NULL;
     }
     return proc;
 }
@@ -466,7 +468,7 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     //    6. call wakup_proc to make the new child process RUNNABLE
     //    7. set ret vaule using child proc's pid
 
-	//LAB5 YOUR CODE : (update LAB4 steps)
+	//LAB5 2011011237 : (update LAB4 steps)
    /* Some Functions
     *    set_links:  set the relation links of process.  ALSO SEE: remove_links:  lean the relation links of process 
     *    -------------------
@@ -478,6 +480,7 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
         goto fork_out;
     }
     proc->parent = current;
+    assert(current->wait_state == 0);
 
     if (setup_kstack(proc) != 0) {
         goto bad_fork_cleanup_proc;
@@ -489,11 +492,14 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
 
     copy_thread(proc, stack, tf);
 
-    proc->pid = get_pid();
-
-    hash_proc(proc);
-    list_add(&proc_list, &(proc->list_link));
-    nr_process ++;
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+        proc->pid = get_pid();
+        hash_proc(proc);
+        set_links(proc);
+    }
+    local_intr_restore(intr_flag);
 
     wakeup_proc(proc);
     ret = proc->pid;
